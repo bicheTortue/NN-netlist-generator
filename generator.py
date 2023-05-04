b4adfd75ab9eb9376b168524f03dab494a2d6ae4
Valentin BARBAZA <barbaza.valentin@gmail.com>
2023-05-04 12:28:26 +0100

    Issue importing verilog-A models, will figure out what to do

--- generator.py
+++ generator.py
@@ -3,13 +3,26 @@
 import argparse
 import sys
 
-
-
+# Defining the different lines/modules
 def header(module_name):
     out.write(".subckt " + module_name + "\n")
 
 def footer(module_name):
     out.write(".ends " + module_name + "\n")
+
+def resistor(name, minus, plus, value):
+    out.write("R" + name + " " + minus + " " + plus + " " + value + "\n")
+
+def MOSFET(name, tType, drain, gate, source, bulk):
+    out.write("M" + name + " " + drain + " " + gate + " " + source + " " + bulk + " " + tType + "\n")
+
+def sigmoid(name, Vin, Vout, V1, V2, V3, idc, gnd, vdd):
+    out.write("X" + name + " " + V1 + " " + V2 + " " + V3 + " " + Vin + " " + Vout + " " + gnd +" " + idc +" " + vdd + "sigmoid\n")
+
+def tanh(name, Vin, Vout, V1, V2, V3, idc, gnd, vdd):
+    out.write("X" + name + " " + V1 + " " + V2 + " " + V3 + " " + Vin + " " + Vout + " " + gnd +" " + idc +" " + vdd + "tanh\n")
+
+# def voltMult
 
 def main():
 
@@ -25,6 +38,10 @@ def main():
     name = (out.name.split('.')[0]) 
     # Start writing the file
     header(name) 
+
+    MOSFET("test","nch","1","2", "3","4")
+    MOSFET("test","pch","1","2", "3","4")
+    resistor("truc", "net1", "net2", "15000")
 
     # End of the file
     footer(name) 
