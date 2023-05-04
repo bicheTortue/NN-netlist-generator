4313bdd0367c15d0d0b17c5c0fddef71a4063a30
Valentin BARBAZA <barbaza.valentin@gmail.com>
2023-05-04 16:59:53 +0100

    XBar function done, about to test

--- generator.py
+++ generator.py
@@ -2,6 +2,13 @@
 
 import argparse
 import sys
+from itertools import count
+
+def getResId(_resCount=count()):
+    return next(_resCount)
+
+def getNetId(_netCount=count()):
+    return next(_netCount)
 
 # Defining the different lines/modules
 def header(module_name):
@@ -11,6 +18,8 @@ def footer(module_name):
     out.write(".ends " + module_name + "\n")
 
 def resistor(name, minus, plus, value):
+    if(type(value) == int):
+        value=str(value)
     out.write("R" + name + " " + minus + " " + plus + " " + value + "\n")
 
 def MOSFET(name, tType, drain, gate, source, bulk):
@@ -22,7 +31,30 @@ def sigmoid(name, Vin, Vout, V1, V2, V3, idc, gnd, vdd):
 def tanh(name, Vin, Vout, V1, V2, V3, idc, gnd, vdd):
     out.write("X" + name + " " + V1 + " " + V2 + " " + V3 + " " + Vin + " " + Vout + " " + gnd +" " + idc +" " + vdd + "tanh\n")
 
-# def voltMult
+def voltMult(name, in1, in2, out):
+    out.write("X"+ name + " " + in1 + " " + in2 + " " + out + "voltageMult\n")
+
+def opAmp(name, pin, nin, out):
+    out.write("X"+ name + " " + nin + " " + out + " " + pin + "opAmp\n")
+
+def memcell(name, inVal, enableIn, enableOut, out):
+    out.write("X"+ name + " " + enableIn + " "  + enableOut + " " + inVal + " " + out + "opAmp\n")
+
+def genMainXBar(nbIn,nbHid,nbOut):
+    for i in range(nbOut):
+        posWeight = str(getNetId())
+        negWeight = str(getNetId())
+        # Setting the input weights
+        for j in range(nbIn):
+            resistor(str(getResId()),"netIn"+str(j), posWeight, 100) # TODO : be able to choose between one or two opAmp
+            resistor(str(getResId()),"netIn"+str(j), negWeight,100)
+        # Setting the hidden weights
+        for j in range(nbHid):
+            resistor(str(getResId()),"netHid"+str(j), posWeight,10)
+            resistor(str(getResId()),"netHid"+str(j), negWeight,10)
+        # Setting the bias weights
+        resistor(str(getResId()),"netBias", posWeight,10)
+        resistor(str(getResId()),"netBias", negWeight,10)
 
 def main():
 
@@ -41,7 +73,12 @@ def main():
 
     MOSFET("test","nch","1","2", "3","4")
     MOSFET("test","pch","1","2", "3","4")
-    resistor("truc", "net1", "net2", "15000")
+    resistor("truc", "1", "2", "15000")
+
+    genMainXBar(1,4,1)
+    genMainXBar(1,4,1)
+    genMainXBar(1,4,1)
+    genMainXBar(1,4,1)
 
     # End of the file
     footer(name) 
