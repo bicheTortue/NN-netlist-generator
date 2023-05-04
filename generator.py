0b8a3e88636c3f238f61f6cc33280c4f53501a86
Valentin BARBAZA <barbaza.valentin@gmail.com>
2023-05-04 10:59:19 +0100

    gitignore plus parser

--- generator.py
+++ generator.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+import argparse
+import sys
 
+
+
+def header(module_name):
+    print(".subckt " + module_name)
+    out.write(".subckt " + module_name+ "\n")
+
+def footer(module_name):
+    print(".ends " + module_name)
 
 def main():
-    print("test")
+
+    parser = argparse.ArgumentParser(prog = 'Analog LSTM Generator', description = 'This program is used to generate spice netlists to be used in Cadence\'s virtuoso. It sets all the memristors values from the weights.')
+    parser.add_argument("-o", "--output", nargs='?', type=argparse.FileType("w"), default=sys.stdout, help="Specify an output file.")
+
+
+    args = parser.parse_args()
+    
+    global out
+    out = args.output
+
+    header("testouille") 
 
 if(__name__ == "__main__"):
     main()
